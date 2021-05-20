76cd4a9530a4501b1b470daf99ce0c29fbd6fbff
= <=>
2021-05-20 09:02:37 +0200

    o

--- lib/common/screens/all/HomeScreen/homescreen.dart
+++ lib/common/screens/all/HomeScreen/homescreen.dart
@@ -5,11 +5,16 @@ import 'components/topbar.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(
         children: [
           // Generated code for this Row Widget...
-          TopBar()
+          TopBar(),
+          Container(
+            width: size.width * 0.9,
+            height: size.height * 0.2,
+          )
         ],
       ),
     );
